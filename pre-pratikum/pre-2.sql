0f59df5445ac84d5e28ffefb57a5909ad5b28eb9
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2025-04-14 10:49:57 +0800

    Add files via upload

---
+++ pre-pratikum/pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Bayu Tri Anugrah
+-- NIM : 23241021
+-- KELAS : A
+
+-- melihat Database yang digunakan
+USE mysql;
+
+-- mendeskripsikan tabel user 
+DESCRIBE user;
+
+-- menampilkan tabel dalam data base mysql
+SHOW TABLES;
+
+-- mendeskripsikan tabel user
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom Host,User,Password
+SELECT Host , User , Pasword FROM user;
